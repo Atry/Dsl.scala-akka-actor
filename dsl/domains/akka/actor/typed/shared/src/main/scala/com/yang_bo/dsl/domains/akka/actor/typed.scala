078138d70254393915abc5d1973c9792433d1545
Yang Bo <atryyang@thoughtworks.com>
2019-06-23 04:12:26 -0700

    Improve Scaladoc

--- dsl/domains/akka/actor/typed/shared/src/main/scala/com/yang_bo/dsl/domains/akka/actor/typed.scala
+++ dsl/domains/akka/actor/typed/shared/src/main/scala/com/yang_bo/dsl/domains/akka/actor/typed.scala
@@ -24,7 +24,7 @@ import scala.reflect.ClassTag
   *
   *          The following state machine contains two states and two transitions between them.
   *
-  *          <p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Finite_state_machine_example_with_comments.svg/420px-Finite_state_machine_example_with_comments.svg.png"/></p>
+  *          <br/><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Finite_state_machine_example_with_comments.svg/420px-Finite_state_machine_example_with_comments.svg.png"/><br/>
   *
   *          It can be created as a simple `while` loop with the help of [[keywords.akka.actor.ReceiveMessage.Partial]]:
   *
